c8fa74b6d783d4da6993b87d05511a3363774ca9
Mohammad Moustafa <mohammad.m@live.ca>
2020-05-10 19:07:06 -0400

    Added some markdown, styling, and tooltips

--- src/components/RecipePage.tsx
+++ src/components/RecipePage.tsx
@@ -4,6 +4,7 @@ import '../style/RecipePage.css';
 import * as conversions from '../store/UnitConversions.json';
 import breakfast from '../assets/breakfast.jpeg';
 import { useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 import {
     Row,
@@ -15,14 +16,19 @@ import {
     InputGroupText,
 } from "shards-react";
 
-type catOptions = {
-    [key: string]: JSX.Element
-  }
-var categories : catOptions = {
+var categories : any = {
     appetizer: <Badge pill className="recipe-badge" theme="primary" key="appetizer">Appetizer</Badge>,
     entree: <Badge pill className="recipe-badge" theme="success" key="entree">Entree</Badge>,
     dessert: <Badge pill className="recipe-badge" theme="info" key="dessert">Dessert</Badge>
 }
+
+var servingOptions: any = [
+  { value: 0.5, label: "2x" },
+  { value: 1, label: "Original"},
+  { value: 2, label: "1/2"},
+  { value: 3, label: "1/3"},
+  { value: 4, label: "1/4"}
+]
 
 
 export default function RecipePage(props: any) {
@@ -61,7 +67,7 @@ export default function RecipePage(props: any) {
     const convertUnit = (num: number, unit: string, name: string) => {
       let divisor : number = divider;
       let clean = ((num / divisor) % 0.25) === 0;
-      if (clean) return `${num/divisor} ${unit}`;
+      if (clean) return `${num/divisor} ${unit} ${name}`;
       else {
         switch(unit) {
           case 'cup':
@@ -73,9 +79,12 @@ export default function RecipePage(props: any) {
             if (tsp > 0) result = result.concat(`${tsp} tsp`);
             return `${result} ${name}`;
           case 'tsp':
-            let res = num/divisor;
+            var res = num/divisor;
             if ([0.25, 0.5, 0.75].includes(res)) return `${res} tsp ${name}`;
             return `pinch of ${name}`;
+          case 'tbsp':
+            var res = num/divisor;
+            if ([2/3, 1/3].includes(res)) return `${Math.round((res + Number.EPSILON) * 100) / 100} tbsp ${name}`
           case 'g':
             return `${Math.floor(num/divisor)} g ${name}`
           default:
@@ -95,25 +104,30 @@ export default function RecipePage(props: any) {
             </Col>
             <Col className="w-100" style={{ paddingLeft: "15px", paddingTop: "15px" }}>
               <Row className="title">{recipe.title}</Row>
-              <Row> 
-                {recipe.prepTime && `Prep Time: ${recipe.prepTime}`}
-              </Row>
               <Row>
-                {recipe.cookTime && `Cook Time: ${recipe.cookTime}`}
+                <Col>
+                  <Row> 
+                    {recipe.prepTime && `Prep Time: ${recipe.prepTime}`}
+                  </Row>
+                  <Row>
+                    {recipe.cookTime && `Cook Time: ${recipe.cookTime}`}
+                  </Row>
+                </Col>
               </Row>
               <Row>{ categories[recipe.category] }</Row>
               <Row className="w-100" style={{ marginTop: "5px"}}>
                 <InputGroup>
                   <InputGroupAddon type="prepend">
-                    <InputGroupText >Divide Recipe</InputGroupText>
+                    <InputGroupText >Change Amount</InputGroupText>
                   </InputGroupAddon>
                   <FormSelect onChange={(e: any) => setDivider(e.target.value)}
                   id="divider"
                   value={divider}>
-                    <option value={1}>Original</option>
-                    <option value={2}>1/2</option>
-                    <option value={4}>1/4</option>
-                    <option value={3}>1/3</option>
+                    {
+                      servingOptions.map((opt: any, i: number) => {
+                        return ( <option key={i} value={opt.value}>{opt.label}</option> )
+                      })
+                    }
                   </FormSelect>
                 </InputGroup>
                 
@@ -124,7 +138,7 @@ export default function RecipePage(props: any) {
             <Col md="4"><h4>Ingredients</h4></Col>
             <Col md="8"><h4>Directions</h4></Col>
           </Row>
-          <Row className="pl-15">
+          <Row className="pl-15 pr-15">
             {/* ingredients */}
             <Col md="4">
               <ul >
@@ -159,7 +173,7 @@ export default function RecipePage(props: any) {
               </Row>
               <Row className="pl-15">
                 <Col>
-                  {recipe.notes}
+                  <ReactMarkdown source={recipe.notes} />
                 </Col>
               </Row> 
             </React.Fragment>
